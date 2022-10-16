6bb4c3995ed36e5fd2e6b49dc643e0266017cdf0
Juliawati Hutagaol <89821421+juliahutagaol22@users.noreply.github.com>
2022-10-16 16:32:38 +0700

    Upload Code

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MainContainer from './src/MainContainer'
+const App = () => {
+  return (
+    <MainContainer />
+  )
+}
+
+export default App
